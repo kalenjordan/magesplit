39828c2861df88a5367cdf93543def7fbe215362
Kalen Jordan <kalen@sparcksoft.com>
2013-10-14 16:20:35 -0700

    Write orders to file with cookie information
    Google Analytics doesn't seem to be very accurate for recording
    conversions using custom events, so I'm recording them in a text
    file

---
+++ app/code/community/KJ/MageSplit/Model/Observer.php
@@ -0,0 +1,39 @@
+<?php
+
+class KJ_MageSplit_Model_Observer
+{
+    /**
+     * @param $observer Varien_Event_Observer
+     */
+    public function salesOrderPlaceAfter($observer)
+    {
+        /** @var Mage_Sales_Model_Order $order */
+        $order = $observer->getData('order');
+
+        $magesplitCookies = array();
+        foreach ($_COOKIE as $key => $val) {
+            if (strpos($key, 'magesplit_') !== false) {
+                $magesplitCookies[$key] = $val;
+            }
+        }
+
+        $line = $order->getIncrementId();
+        foreach ($magesplitCookies as $key => $val) {
+            $line .= " - $key: $val";
+        }
+
+        $this->_writeLineToFile($line);
+
+        throw new Exception("roll back");
+    }
+
+    protected function _writeLineToFile($line)
+    {
+        $dataDir = Mage::getModuleDir('', 'KJ_MageSplit') . '/data';
+        $filePath = $dataDir . '/order.txt';
+
+        file_put_contents($filePath, $line . "\n", FILE_APPEND);
+
+        return $this;
+    }
+}
